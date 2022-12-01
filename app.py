d2aa48679c4dfb11bbe0b74f67ff3763451a86dc
Divyansh200518 <87868119+Divyansh200518@users.noreply.github.com>
2022-12-01 20:55:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,4 +38,4 @@ def search():
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True,host="0.0.0.0", port=os.getenv("PORT", default=5000))
